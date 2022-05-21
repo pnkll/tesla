54038772fb3acb8046ea281324e31398682eae75
pnkll <kavasakya@gmail.com>
2022-05-21 20:10:45 +0300

    start

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Parser } from './parser/parser'
+import axios from 'axios';
 
 function App() {
+
+  const start = async () =>{
+     const response = await axios.get('https://teslahelp.ru/catalog/model-3-parts-catalog/')
+     console.log(response)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Tesla catalog
+        <button onClick={start()}>Hello</button>
       </header>
     </div>
   );
---
+++ src/parser/parser.js
@@ -0,0 +1,11 @@
+// export const Parser = () => {
+//     const sayHello = () => {
+//         console.log('hello')
+//     }
+// }
+
+export const Parser = {
+    sayHello(){
+        console.log('hello')
+    }
+}
